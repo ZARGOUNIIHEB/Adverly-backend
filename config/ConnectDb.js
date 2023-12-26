e8346e48632fe45e931400d9f244a3f2ebbdc969
Iheb ZARGOUNI <iheb.zargouni.pr@gmail.com>
2023-12-26 14:57:08 +0100

    new commit

--- config/ConnectDb.js
+++ config/ConnectDb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ConnectDb = async () => {
     try {
-        await mongoose.connect('mongodb+srv://ihebzargounipr:jyntZp0uxvXzeTqS@cluster0.jjgehee.mongodb.net/e_annonce?retryWrites=true&w=majority');
+        await mongoose.connect('mongodb+srv://**********:**********@cluster0.jjgehee.mongodb.net/e_annonce?retryWrites=true&w=majority');
         console.log('You are connected to your database EAnnonce');
     } catch (err) {
         console.log(err)
